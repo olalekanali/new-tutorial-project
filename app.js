5dde29d75f37c226498b7c20b6d9c46ad8fcc453
Ali <aoaliyu@student.oauife.edu.ng>
2025-01-06 12:51:30 +0100

    second commit

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const http = require('http');
+const express = require('express');
 
 const server = http.createServer((req, res)=>{
     if(req.url === '/'){
         res.end(`Hello Developers`);
+        console.log('Hello world');
+        
     } else {
         res.end(`Hello World`);
     }
